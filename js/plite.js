510b2b0272d6f6448462fffca9bf92c4fa23fe34
Fabian Frei <fabian.frei@esf-frei.ch>
2015-09-01 16:35:18 +0200

    added promise library

---
+++ js/plite.js
@@ -0,0 +1,142 @@
+function Plite(resolver) {
+  var emptyFn = function () {},
+      chain = emptyFn,
+      resultGetter;
+
+  function processResult(result, callback, reject) {
+    if (result && result.then) {
+      result.then(function (data) {
+        processResult(data, callback, reject);
+      }).catch(function (err) {
+        processResult(err, reject, reject);
+      });
+    } else {
+      callback(result);
+    }
+  }
+
+  function setResult(callbackRunner) {
+    resultGetter = function (successCallback, failCallback) {
+      try {
+        callbackRunner(successCallback, failCallback);
+      } catch (ex) {
+        failCallback(ex);
+      }
+    };
+
+    chain();
+    chain = undefined;
+  }
+
+  function setError(err) {
+    setResult(function (success, fail) {
+      fail(err);
+    });
+  }
+
+  function setSuccess(data) {
+    setResult(function (success) {
+      success(data);
+    });
+  }
+
+  function buildChain(onsuccess, onfailure) {
+    var prevChain = chain;
+    chain = function () {
+      prevChain();
+      resultGetter(onsuccess, onfailure);
+    };
+  }
+
+  var self = {
+    then: function (callback) {
+      var resolveCallback = resultGetter || buildChain;
+
+      return Plite(function (resolve, reject) {
+        resolveCallback(function (data) {
+          resolve(callback(data));
+        }, reject);
+      });
+    },
+
+    catch: function (callback) {
+      var resolveCallback = resultGetter || buildChain;
+
+      return Plite(function (resolve, reject) {
+        resolveCallback(resolve, function (err) {
+          reject(callback(err));
+        });
+      });
+    },
+
+    resolve: function (result) {
+      !resultGetter && processResult(result, setSuccess, setError);
+    },
+
+    reject: function (err) {
+      !resultGetter && processResult(err, setError, setError);
+    }
+  };
+
+  resolver && resolver(self.resolve, self.reject);
+
+  return self;
+}
+
+Plite.resolve = function (result) {
+  return Plite(function (resolve) {
+    resolve(result);
+  });
+};
+
+Plite.reject = function (err) {
+  return Plite(function (resolve, reject) {
+    reject(err);
+  });
+};
+
+Plite.race = function (promises) {
+  promises = promises || [];
+  return Plite(function (resolve, reject) {
+    var len = promises.length;
+    if (!len) return resolve();
+
+    for (var i = 0; i < len; ++i) {
+      var p = promises[i];
+      p && p.then && p.then(resolve).catch(reject);
+    }
+  });
+};
+
+Plite.all = function (promises) {
+  promises = promises || [];
+  return Plite(function (resolve, reject) {
+    var len = promises.length,
+        count = len;
+
+    if (!len) return resolve();
+
+    function decrement() {
+      --count <= 0 && resolve(promises);
+    }
+
+    function waitFor(p, i) {
+      if (p && p.then) {
+        p.then(function (result) {
+          promises[i] = result;
+          decrement();
+        }).catch(reject);
+      } else {
+        decrement();
+      }
+    }
+
+    for (var i = 0; i < len; ++i) {
+      waitFor(promises[i], i);
+    }
+  });
+};
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  module.exports = Plite;
+}
